7561820ccd565e97a0b0ba263d2314d677a86ae6
joyunjea <92347725+joyunjea@users.noreply.github.com>
2022-01-09 02:37:18 +0900

    spring

--- spring_project/Spring-MVC/src/main/java/com/bitcamp/mvc/ExceptionController.java
+++ spring_project/Spring-MVC/src/main/java/com/bitcamp/mvc/ExceptionController.java
@@ -1,12 +1,13 @@
 package com.bitcamp.mvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ExceptionController {
-	
+	@Autowired
 	private HelloService service;
 	
 	@RequestMapping("/error/null")
